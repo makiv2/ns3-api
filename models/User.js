d05fb88b2f1270820843cb9c37b3246c32ac6d17
Matija Popovic <matijap@stud.ntnu.no>
2022-10-29 02:50:52 +0200

    user model

--- models/User.js
+++ models/User.js
@@ -1,9 +1,51 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const secret = require("../config").secret;
 
-const UserSchema = mongoose.Schema({
-    username: String,
-    password: String,
-    email: String,
-});
+
+//Todo fix jwt token
+
+UserSchema.methods.setPassword = function(password) {
+    this.salt = crypto.randomBytes(16).toString("hex");
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, "sha512").toString("hex");
+};
+
+
+
+UserSchema.methods.validPassword = function(password) {
+    const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, "sha512").toString("hex");
+    return this.hash === hash;
+};
+
+
+const UserSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      required: [true, "can't be blank"],
+      match: [/^[a-zA-Z0-9]+$/, "is invalid"],
+      index: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      required: [true, "can't be blank"],
+      match: [/\S+@\S+\.\S+/, "is invalid"],
+      index: true,
+    },
+    image: String,
+    hash: String,
+    salt: String,
+  },
+  { timestamps: true }
+);
+
+
+UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
 
 module.exports = mongoose.model("User", UserSchema);
