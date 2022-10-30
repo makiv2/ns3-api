b4a099e1687cecb23ec85d303f30e867533ea889
Matija Popovic <matijap@stud.ntnu.no>
2022-10-30 02:52:33 +0200

    JWT in usershema

--- models/User.js
+++ models/User.js
@@ -2,10 +2,37 @@ const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
-const secret = require("../config").secret;
-
+require("dotenv").config();
 
 //Todo fix jwt token
+
+UserSchema.methods.generateJWT = function() {
+  const today = new Date();
+  const exp = new Date(today);
+  exp.setDate(today.getDate() + 60);
+
+  return jwt.sign(
+    {
+      id: this._id,
+      username: this.username,
+      exp: parseInt(exp.getTime() / 1000)
+    },
+    process.env.SECRET
+  );
+};
+
+
+UserSchema.methods.toAuthJSON = function() {
+  return {
+    username: this.username,
+    email: this.email,
+    token: this.generateJWT()
+  };
+};
+
+
+
+
 
 UserSchema.methods.setPassword = function(password) {
     this.salt = crypto.randomBytes(16).toString("hex");
